59a6bb1f8578733324f453b7b1907ea571d9edd8
642609666 <642609666@qq.com>
2017-01-17 15:00:25 +0800

    7.视频,图片,文字,gif的ViewHolder

--- app/src/main/java/com/example/administrator/movie321/adapter/NetAudioFragmentAdapter.java
+++ app/src/main/java/com/example/administrator/movie321/adapter/NetAudioFragmentAdapter.java
@@ -9,10 +9,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.administrator.movie321.R;
 import com.example.administrator.movie321.bean.NetAudioBean;
+import com.example.administrator.movie321.utils.DensityUtil;
 import com.example.administrator.movie321.utils.Utils;
 
+import org.xutils.image.ImageOptions;
 import org.xutils.x;
 
 import java.util.List;
@@ -299,39 +302,80 @@ public class NetAudioFragmentAdapter extends BaseAdapter {
         }
     }
 
-    class ImageHolder {
-        private TextView textView;
+    class ImageHolder extends BaseViewHolder {
+        TextView tvContext;
+        ImageView ivImageIcon;
 
-        public ImageHolder(View convertView) {
-            textView = (TextView) convertView.findViewById(R.id.textView);
+        ImageHolder(View convertView) {
+            super(convertView);
+            //中间公共部分 -所有的都有
+            tvContext = (TextView) convertView.findViewById(R.id.tv_context);
+            ivImageIcon = (ImageView) convertView.findViewById(R.id.iv_image_icon);
+
         }
 
         public void setData(NetAudioBean.ListBean mediaItem) {
-            textView.setText("我是图片的内容");
+            super.setData(mediaItem);
+            //设置文本-所有的都有
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+            //图片特有的
+
+            ivImageIcon.setImageResource(R.drawable.bg_item);
+            if (mediaItem.getImage() != null && mediaItem.getImage() != null && mediaItem.getImage().getSmall() != null) {
+                Glide.with(mContext).load(mediaItem.getImage().getDownload_url().get(0)).placeholder(R.drawable.bg_item).error(R.drawable.bg_item).diskCacheStrategy(DiskCacheStrategy.ALL).into(ivImageIcon);
+            }
         }
     }
 
-    class TextHolder {
-        private TextView textView;
+    class TextHolder extends BaseViewHolder {
+        TextView tvContext;
 
-        public TextHolder(View convertView) {
-            textView = (TextView) convertView.findViewById(R.id.textView);
+        TextHolder(View convertView) {
+            super(convertView);
+            //中间公共部分 -所有的都有
+            tvContext = (TextView) convertView.findViewById(R.id.tv_context);
         }
 
         public void setData(NetAudioBean.ListBean mediaItem) {
-//            textView.setText("我是文本的内容");
+            super.setData(mediaItem);
+            //设置文本-所有的都有
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
         }
     }
 
-    class GifHolder {
-        private TextView textView;
+    class GifHolder extends BaseViewHolder {
+        TextView tvContext;
+        ImageView ivImageGif;
+        private ImageOptions imageOptions;
 
-        public GifHolder(View convertView) {
-            textView = (TextView) convertView.findViewById(R.id.textView);
+        GifHolder(View convertView) {
+            super(convertView);
+            //中间公共部分 -所有的都有
+            tvContext = (TextView) convertView.findViewById(R.id.tv_context);
+            ivImageGif = (ImageView) convertView.findViewById(R.id.iv_image_gif);
+
+            imageOptions = new ImageOptions.Builder()
+                    //包裹类型
+                    .setSize(ViewGroup.LayoutParams.WRAP_CONTENT, -2)
+                    //设置圆角
+                    .setRadius(DensityUtil.dip2px(mContext, 5))
+                    .setIgnoreGif(false)//是否忽略gif图。false表示不忽略。不写这句，默认是true
+                    .setImageScaleType(ImageView.ScaleType.CENTER_CROP)
+                    .setLoadingDrawableId(R.drawable.video_default)
+                    .setFailureDrawableId(R.drawable.video_default)
+                    .build();
         }
 
         public void setData(NetAudioBean.ListBean mediaItem) {
-            textView.setText("我是GIF的内容");
+            super.setData(mediaItem);
+            //设置文本-所有的都有
+            tvContext.setText(mediaItem.getText() + "_" + mediaItem.getType());
+
+            //下面是gif
+            if (mediaItem.getGif() != null && mediaItem.getGif() != null && mediaItem.getGif().getImages() != null) {
+//                Glide.with(context).load(mediaItem.getGif().getImages().get(0)).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(ivImageGif);
+                x.image().bind(ivImageGif, mediaItem.getGif().getImages().get(0), imageOptions);
+            }
         }
     }
 
