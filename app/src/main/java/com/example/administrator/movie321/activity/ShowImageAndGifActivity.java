a19ebe178403326aee2bfcde3458a18b633cc268
642609666 <642609666@qq.com>
2017-01-17 15:12:33 +0800

    9.引入photoView

---
+++ app/src/main/java/com/example/administrator/movie321/activity/ShowImageAndGifActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.movie321.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.movie321.R;
+
+public class ShowImageAndGifActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_image_and_gif);
+    }
+}
--- app/src/main/java/com/example/administrator/movie321/fragment/NetMusicFragment.java
+++ app/src/main/java/com/example/administrator/movie321/fragment/NetMusicFragment.java
@@ -1,12 +1,15 @@
 package com.example.administrator.movie321.fragment;
 
+import android.content.Intent;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.administrator.movie321.R;
+import com.example.administrator.movie321.activity.ShowImageAndGifActivity;
 import com.example.administrator.movie321.adapter.NetAudioFragmentAdapter;
 import com.example.administrator.movie321.base.BaseFragment;
 import com.example.administrator.movie321.bean.NetAudioBean;
@@ -36,11 +39,38 @@ public class NetMusicFragment extends BaseFragment {
     @Bind(R.id.tv_nomedia)
     TextView tvNomedia;
     private NetAudioFragmentAdapter myAdapter;
+    List<NetAudioBean.ListBean> datas;
 
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.fragment_net_audio, null);
         ButterKnife.bind(this, view);
+
+        //设置点击事件
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                NetAudioBean.ListBean listEntity = datas.get(position);
+                if(listEntity !=null ){
+                    //3.传递视频列表
+                    Intent intent = new Intent(mContext,ShowImageAndGifActivity.class);
+                    if(listEntity.getType().equals("gif")){
+                        String url = listEntity.getGif().getImages().get(0);
+                        intent.putExtra("url",url);
+                        mContext.startActivity(intent);
+                    }else if(listEntity.getType().equals("image")){
+                        String url = listEntity.getImage().getBig().get(0);
+                        intent.putExtra("url",url);
+                        mContext.startActivity(intent);
+                    }
+                }
+
+
+            }
+        });
+
         return view;
     }
 
@@ -87,7 +117,7 @@ public class NetMusicFragment extends BaseFragment {
         NetAudioBean netAudioBean = paraseJons(json);
         //Log.e("TAG", "+++++" + netAudioBean.getList().get(0).getText());
         //设置适配器
-        List<NetAudioBean.ListBean> datas = netAudioBean.getList();
+         datas = netAudioBean.getList();
         if (datas != null && datas.size() > 0) {
             //有视频
             tvNomedia.setVisibility(View.GONE);
